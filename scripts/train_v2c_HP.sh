e535c08ad05ef404a530fae09e5e0569119c303f
indigopyj <indigopyj@gmail.com>
2022-05-25 14:30:46 +0000

    hp tuning description

--- scripts/train_v2c_HP.sh
+++ scripts/train_v2c_HP.sh
@@ -8,3 +8,10 @@ python train_RED.py --dataroot /home/nas2_userH/yeojeongpark/compression/dataset
 --act_GAN_loss \
 --Temporal_GAN_loss --lambda_D_T 1.0 \
 --lambda_feature 1.0 \ 
+
+# lambda_L1 : 기준 프레임의 activation과 red가 예측한 difference map을 반영한 activation에 걸리는 l1 loss
+# lambda_L1_out: main generator output과 red output간에 걸리는 l1 loss
+# lambda_lpips : perceptual loss
+# n_convs : red network의 conv block 개수
+# Temporal_GAN_loss: (기준프레임 output, real output)과 (기준프레임 output, fake output)을 discriminator에 넣는 loss
+# labmda_feature : feature matching loss
